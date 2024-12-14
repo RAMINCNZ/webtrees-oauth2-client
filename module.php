a2c232739e72250b0fc454fef5aebd8705a0742a
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2024-12-14 13:55:52 +0100

    Added autoload for the latest version of the common code library to module.php

--- module.php
+++ module.php
@@ -33,18 +33,18 @@
  
 declare(strict_types=1);
 
-use Composer\Autoload\ClassLoader;
-use Jefferson49\Webtrees\Module\OAuth2Client\OAuth2Client;
+namespace Jefferson49\Webtrees\Module\OAuth2Client;
 
-$loader = new ClassLoader();
+use Composer\Autoload\ClassLoader;
+use Composer\InstalledVersions;
+
+
+$loader = new ClassLoader(__DIR__);
 
 //This webtrees custom module
 $loader->addPsr4('Jefferson49\\Webtrees\\Module\\OAuth2Client\\', __DIR__ . '/src');
 
-//Helper functions for webtrees custom modules
-$loader->addPsr4('Jefferson49\\Webtrees\\Helpers\\', __DIR__ . "/vendor/Jefferson49/Webtrees/Helpers/");
-
-//league/oauth2-clients
+//league/oauth2 clients
 $loader->addPsr4('League\\OAuth2\\Client\\', __DIR__ . '/vendor/league/oauth2-client/src');
 $loader->addPsr4('League\\OAuth2\\Client\\', __DIR__ . '/vendor/league/oauth2-github/src');
 $loader->addPsr4('League\\OAuth2\\Client\\', __DIR__ . '/vendor/league/oauth2-google/src');
@@ -57,5 +57,27 @@ require __DIR__ . '/src/Provider/GithubAuthorizationProvider.php';
 require __DIR__ . '/src/Provider/GoogleAuthorizationProvider.php';
 require __DIR__ . '/src/Provider/JoomlaAuthorizationProvider.php';
 require __DIR__ . '/src/Provider/WordPressAuthorizationProvider.php';
+
+//Autoload the latest version of the common code library, which is shared between webtrees custom modules
+$loader = new ClassLoader(__DIR__ .'/vendor');
+
+try {
+    $autoload_common_library_version = InstalledVersions::getVersion('jefferson49/webtrees-common');
+}
+catch (\OutOfBoundsException $e) {
+    $autoload_common_library_version = '';
+}
+
+$local_composer_versions = require __DIR__ . '/vendor/composer/installed.php';
+$local_common_library_version = $local_composer_versions['versions']['jefferson49/webtrees-common']['version'];
+
+//If the found library is later than the current autoload version, prepend the found library to autoload
+//This ensures that always the latest library version is autoloaded
+if (version_compare($local_common_library_version, $autoload_common_library_version, '>')) {
+    $loader->addPsr4('Jefferson49\\Webtrees\\Helpers\\', __DIR__ . '/vendor/jefferson49/webtrees-common/Helpers');
+    $loader->addPsr4('Jefferson49\\Webtrees\\Internationalization\\', __DIR__. '/vendor/jefferson49/webtrees-common/Internationalization');    
+    $loader->addPsr4('Jefferson49\\Webtrees\\Log\\', __DIR__ . '/vendor/jefferson49/webtrees-common/Log');
+    $loader->register(true);    
+}
 
 return new OAuth2Client();
